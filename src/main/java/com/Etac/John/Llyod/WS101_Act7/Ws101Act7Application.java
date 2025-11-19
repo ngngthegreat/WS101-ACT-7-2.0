abba782c1e97491c8b2419dfe42cf470d376916f
Etac John Llyod <johnllyod94@gmail.com>
2025-11-19 14:47:55 +0800

    Added full Spring Boot project for Lab 7

---
+++ src/main/java/com/Etac/John/Llyod/WS101_Act7/Ws101Act7Application.java
@@ -0,0 +1,13 @@
+package com.Etac.John.Llyod.WS101_Act7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws101Act7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws101Act7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Etac/John/Llyod/WS101_Act7/Ws101Act7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Etac.John.Llyod.WS101_Act7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws101Act7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
